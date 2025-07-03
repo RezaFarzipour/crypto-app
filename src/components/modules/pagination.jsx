10ec12bc9ee94645415b2873cfb66a17ce5fb0eb
RezaFarzipour <rezafafa78@gmail.com>
2025-07-03 17:43:07 +0330

    pagination completed

--- src/components/modules/pagination.jsx
+++ src/components/modules/pagination.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import styles from './pagination.module.css'
 const Pagination = ({page,setPage}) => {
 
 
@@ -13,26 +13,26 @@ const Pagination = ({page,setPage}) => {
     setPage((page) => page + 1);
   };
 
-  console.log('pp',page);
+
   
   return (
-    <div>
-      <button onClick={previousHandler}>previous</button>
-      <p style={{color:page ===1 ? 'red': "inherit"}}>1</p>
+    <div className={styles.pagination}>
+      <button onClick={previousHandler} className={page ===1 ? styles.disabled:null}>previous</button>
+      <p  className={page ===1 ? styles.selected : null}>1</p>
      
-      <p>2</p>
+      <p className={page ===2 ? styles.selected : null}>2</p>
 
       {page > 2 && page <9 && (
         <>
          <span>...</span>
-         <p>{page}</p>
+         <p className={ styles.selected }>{page}</p>
         </>
       )}
       <span>...</span>
-      <p>9</p>
-      <p>10</p>
+      <p className={page ===9 ? styles.selected : null}>9</p>
+      <p className={page ===10 ? styles.selected : null}>10</p>
 
-      <button onClick={nexthandler}>next</button>
+      <button className={page ===10 ? styles.disabled:null} onClick={nexthandler}>next</button>
     </div>
   );
 };
