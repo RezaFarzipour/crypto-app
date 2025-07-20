7d475d1438684ab73b2bb48a8d593bd0362a6a7d
RezaFarzipour <rezafafa78@gmail.com>
2025-07-21 00:07:00 +0330

    setinterval added to datafetching

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -7,44 +7,40 @@ import Chart from "../modules/Chart";
 
 const HomePage = () => {
   const [coins, setCoins] = useState([]);
-  const [isLoading,setIsloading] = useState(true)
+  const [isLoading, setIsloading] = useState(true);
   const [page, setPage] = useState(1);
-  const [currency,setCurrency] = useState("usd")
-  const [chart,setChart] = useState(null)
-
-
+  const [currency, setCurrency] = useState("usd");
+  const [chart, setChart] = useState(null);
 
   useEffect(() => {
-    setIsloading(true)
-    
-    const getdata = async()=>{
+    setIsloading(true);
 
+    const getData = async () => {
       try {
-        const res= await fetch(getCoinList(page,currency))
-        const data= await res.json()
-        setCoins(data)
-        setIsloading(false)
+        const res = await fetch(getCoinList(page, currency));
+        const data = await res.json();
+        setCoins(data);
+        setIsloading(false);
       } catch (error) {
         console.log(error);
-        
       }
+    };
 
-    }
+    getData();
 
-     
-     getdata()
+    const interval = setInterval(() => {
+      getData();
+    }, 10000);
 
-  }, [page,currency]);
-
-
-  
+    return () => clearInterval(interval);
+  }, [page, currency]);
 
   return (
     <div>
-      <Search setCurrency={setCurrency} currency={currency}/>
-      <Tablecoin coins={coins} isLoading={isLoading}  setChart={setChart}/>
-      <Pagination page={page} setPage={setPage}/>
-      {!!chart && <Chart chart={chart} setChart={setChart}/>}
+      <Search setCurrency={setCurrency} currency={currency} />
+      <Tablecoin coins={coins} isLoading={isLoading} setChart={setChart} />
+      <Pagination page={page} setPage={setPage} />
+      {!!chart && <Chart chart={chart} setChart={setChart} />}
     </div>
   );
 };
